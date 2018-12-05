e678137215790158d1d625b302e2143cd60bf99b
RichardThompson8 <44169456+RichardThompson8@users.noreply.github.com>
2018-12-05 16:21:38 +0000

    Separate kill and reproduce in model class

--- game_theory/analysis.py
+++ game_theory/analysis.py
@@ -9,8 +9,8 @@ def fft_analysis(model):
     # surprisingly this iterates over columns, not rows
     for population_data in all_population_data:
         N = len(all_population_data[population_data])
-        print(N)
-        print(all_population_data[population_data])
+#        print(N)
+#        print(all_population_data[population_data])
         t_axis = np.linspace(0.0, 1.0 / (2.0), (int(N) / 2))
         y_axis = all_population_data[population_data] - np.mean(all_population_data[population_data])
         y_axis_fft = scipy.fftpack.fft(y_axis)
@@ -31,11 +31,48 @@ def fft_analysis(model):
     plt.show()
     print("Dominant frequency >> ", t_corrected[np.argmax(y_corrected)])
 
+def histogram(model):
+    all_population_scores = model.datacollector_population_scores.get_model_vars_dataframe()
+    indexes = all_population_scores.columns
+    current_population_scores = []
+    bins = np.arange((-1) * model.num_moves_per_set * 8, model.num_moves_per_set * 8)
+    for a in indexes:
+        population_scores = all_population_scores[a]
+        current_population_scores.append(population_scores[model.number_of_steps - 1])
+    labels = [a for a in all_population_scores.columns]
+#    print(labels)
+    plt.figure(3,)
+    plt.hist(current_population_scores, bins, label=labels, stacked=True)
+    plt.xlabel("Score")
+    plt.ylabel("Score density")
+    plt.legend(loc="best")
+#    for population_scores in all_population_scores:
+#        print("population scores>>")          
+#        print(all_population_scores[population_scores])
+         
+# =============================================================================
+#     for population_scores in all_population_scores:
+#         print("population scores>>", all_population_scores[population_scores])        
+#         plt.hist(all_population_scores[population_scores], bins=4)
+# =============================================================================
+#bins=model.num_moves_per_set*16+1
+#    plt.show()
 
+def pie_chart(model):
+     all_population_data = model.datacollector_populations.get_model_vars_dataframe()
+     current_populations =[]
+     for population_data in all_population_data:
+         populations = all_population_data[population_data]
+         current_populations.append(populations[model.number_of_steps])
+#     current_populations = [a[model.number_of_steps - 1] for a in [population_data for population_data in all_population_data]]
+     plt.figure(4,)
+     plt.pie(current_populations, labels=[a for a in all_population_data.columns])
+     
+    
+        
 def ternary_plot(model, labels):
     all_population_data = model.datacollector_populations.get_model_vars_dataframe()
     for index, data in all_population_data.iterrows():
-
 
 
         list_norm = [(i / model.height ** 2) for i in all_population_data[population_data]]
@@ -55,12 +92,3 @@ def ternary_plot(model, labels):
     tax.ticks(axis='lbr', multiple=0.2, linewidth=1)
     tax.legend()
     tax.show()
-
-def pie_chart():
-    pass
-
-def histogram():
-    pass
-
-def cross_sectional_graph():
-    pass
